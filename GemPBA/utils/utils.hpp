8cc78e69f8d8e0d36d80f8f5e11453bbb17c0916
Andres Pastrana <rapastranac@gmail.com>
2024-05-03 20:34:58 -0600

    #25 Minor fixes

--- GemPBA/utils/utils.hpp
+++ GemPBA/utils/utils.hpp
@@ -8,10 +8,11 @@
  * Created by Andres Pastrana on 2024-04-08.
  */
 namespace utils {
+
     template<typename ...T>
-    void print_mpi_debug_comments(fmt::format_string<T...> &&formatString, T &&... args) {
+    void print_mpi_debug_comments(const fmt::format_string<T...> &formatString, T &&... args) {
 #ifdef DEBUG_COMMENTS
-        fmt::print(formatString, args...);
+        fmt::print(formatString, std::forward<T>(args)...);
 #endif
     }
 
