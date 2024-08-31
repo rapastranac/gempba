c5bc16c390ea2a56ad5228e9caf4e5fa7fcc1035
Andres Pastrana <rapastranac@gmail.com>
2024-08-31 13:43:26 -0600

    #25 Created utility methods in utils.hpp

---
+++ tests/GemPBA/utils/utils_test.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <future>
+#include <any>
+#include <string>
+#include <stdexcept>
+#include "../../../GemPBA/utils/utils.hpp"
+
+/**
+ * @author Andres Pastrana
+ * @date 2024-08-31
+ */
+
+TEST(UtilsTest, ConvertToAnyFuture_Int) {
+
+    std::promise<int> promise;
+    std::future<int> future = promise.get_future();
+    promise.set_value(42);
+
+    std::future<std::any> anyFuture = utils::convert_to_any_future(std::move(future));
+    std::any result = anyFuture.get();
+
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(std::any_cast<int>(result), 42);
+}
+
+TEST(UtilsTest, ConvertToAnyFuture_String) {
+    std::promise<std::string> promise;
+    std::future<std::string> future = promise.get_future();
+    promise.set_value("hello");
+
+    std::future<std::any> anyFuture = utils::convert_to_any_future(std::move(future));
+    std::any result = anyFuture.get();
+
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(std::any_cast<std::string>(result), "hello");
+}
