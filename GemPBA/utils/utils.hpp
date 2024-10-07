d904a7beeeaef5386a3e813b3b1aaa06c4bf761b
Andres Pastrana <rapastranac@gmail.com>
2024-10-06 19:27:52 -0600

    #25 Given the original buildWaitingList in MPI_Scheduler, created build_topology in utils
    - Wrote test as well

--- GemPBA/utils/utils.hpp
+++ GemPBA/utils/utils.hpp
@@ -7,6 +7,8 @@
 
 #include <spdlog/spdlog.h>
 
+#include "Tree.hpp"
+
 /**
  * Created by Andres Pastrana on 2024-04-08.
  */
@@ -29,6 +31,24 @@ namespace utils {
         return anyFuture;
     }
 
+    // already adapted for multi-branching
+    static int get_next_child(int child, int parent, int children_per_node, int depth) {
+        return child * (int) pow(children_per_node, depth) + parent;
+    }
+
+    static void build_topology(Tree &tree, int parent, int depth_start, int children_per_node, int total) {
+        for (int depth = depth_start; depth < log2(total); depth++) {
+            for (int child = 1; child < children_per_node; child++) {
+                int next_child = get_next_child(child, parent, children_per_node, depth);
+                if (next_child >= total || next_child <= 0) {
+                    continue;
+                }
+                tree[parent].addNext(next_child);
+                spdlog::info("process: {}, child: {}\n", parent, next_child);
+                build_topology(tree, next_child, depth + 1, children_per_node, total);
+            }
+        }
+    }
 };
 
 #endif //UTILS_H
