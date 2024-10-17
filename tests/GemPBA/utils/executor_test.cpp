121bb5771d4bf5cd36a7d1d6959f5ba219514e02
Andres Pastrana <rapastranac@gmail.com>
2024-10-16 20:01:37 -0600

    #25 Created Executor in utils and tests

---
+++ tests/GemPBA/utils/executor_test.cpp
@@ -0,0 +1,111 @@
+#include <gtest/gtest.h>
+
+#include <boost/archive/binary_oarchive.hpp>
+#include <functional>
+#include <sstream>
+
+#include "../../../GemPBA/utils/ExecutorImpl.hpp"
+
+TEST(ExecutorTest, ExecutorImplNonVoid) {
+
+    std::function<double(int, int, float, double, void *)> foo = [](int id, int ivalue, float fvalue, double dvalue, void *dummy) {
+        return static_cast<double>(ivalue) + fvalue + dvalue;
+    };
+
+    gempba::NodeManager &node_manager = gempba::NodeManager::getInstance();
+    node_manager.initThreadPool(1);
+    gempba::ExecutorImpl<double(int, int, float, double, void *)> executorImpl(foo, node_manager);
+
+    // Create an Executor* that should point to executorImpl if it is indeed an implementation
+    auto *executor = dynamic_cast<gempba::Executor<double(int, int, float, double, void *)> *>(&executorImpl);
+    EXPECT_TRUE(executor != nullptr) << "executorImpl is not an instance of Executor";
+
+    int val1 = 1;
+    float val2 = 2.4f;
+    double val3 = 3.54;
+
+    // Create an archive to serialize the values
+    std::ostringstream oss;
+    boost::archive::binary_oarchive oa(oss);
+
+    // Serialize each value into the archive
+    oa << val1;
+    oa << val2;
+    oa << val3;
+
+    // Get the serialized string from the output stream
+    std::string serialized_arguments = oss.str();
+
+    std::shared_future<std::string> f = executorImpl(serialized_arguments);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+    double timeout = 3.0;
+    while (!gempba::is_future_ready(f)) {
+        auto current_time = std::chrono::high_resolution_clock::now();
+        if (std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() > timeout) {
+            FAIL();
+        }
+    }
+
+    const std::string &serialized_result = f.get();
+
+    std::istringstream iss(serialized_result);
+    boost::archive::binary_iarchive ia(iss);
+
+    double deserialized_result;
+
+    ia >> deserialized_result;
+
+    ASSERT_EQ(6.9400000953674317, deserialized_result);
+}
+
+TEST(ExecutorTest, ExecutorImplVoid) {
+
+    std::atomic<double> test_buffer;
+
+    std::function<void(int, int, float, double, void *)> foo = [&test_buffer](int id, int ivalue, float fvalue, double dvalue, void *dummy) {
+        auto value = static_cast<double>(ivalue) + fvalue + dvalue;
+        test_buffer.store(value, std::memory_order_relaxed);
+    };
+
+    gempba::NodeManager &node_manager = gempba::NodeManager::getInstance();
+    node_manager.initThreadPool(1);
+    gempba::ExecutorImpl<void(int, int, float, double, void *)> executorImpl(foo, node_manager);
+
+    // Create an Executor* that should point to executorImpl if it is indeed an implementation
+    auto *executor = dynamic_cast<gempba::Executor<void(int, int, float, double, void *)> *>(&executorImpl);
+    EXPECT_TRUE(executor != nullptr) << "executorImpl is not an instance of Executor";
+
+    int val1 = 1;
+    float val2 = 2.4f;
+    double val3 = 3.54;
+
+    // Create an archive to serialize the values
+    std::ostringstream oss;
+    boost::archive::binary_oarchive oa(oss);
+
+    // Serialize each value into the archive
+    oa << val1;
+    oa << val2;
+    oa << val3;
+
+    // Get the serialized string from the output stream
+    std::string serialized_arguments = oss.str();
+
+    std::shared_future<void> f = executorImpl(serialized_arguments);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+    double timeout = 3.0;
+    std::chrono::nanoseconds elapsed_time;
+    while (!gempba::is_future_ready(f)) {
+        auto current_time = std::chrono::high_resolution_clock::now();
+        if (std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() > timeout) {
+            FAIL();
+        }
+        // update elapsed time
+        elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time - start_time);
+    }
+
+
+    ASSERT_EQ(6.9400000953674317, test_buffer.load());
+}
