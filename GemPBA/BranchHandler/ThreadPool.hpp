91af8e36f46654f86f13d79fdd1cfd58aa3a3130
Andres Pastrana <rapastranac@gmail.com>
2024-10-30 09:41:22 -0600

    #33 Fixed licence credits for CPTL as per Apache 2.0

--- GemPBA/BranchHandler/ThreadPool.hpp
+++ GemPBA/BranchHandler/ThreadPool.hpp
@@ -1,3 +1,21 @@
+/*********************************************************
+*
+*  Copyright (C) 2014 by Vitaliy Vitsentiy
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*********************************************************/
+
 #ifndef OMP_THREADPOOL_H
 #define OMP_THREADPOOL_H
 
@@ -17,12 +35,6 @@
 #include <map>
 #include <set>
 #include <omp.h>
-
-/*
- *Created by Andres Pastrana on 2020-05
- *pasr1602@usherbrooke.ca
- *rapastranac@gmail.com
- */
 
 /*
  * This pool has a fixed size during the whole execution, it's non-copyable, non-deletable, non-movable,
