a66c7a2172d9ae3e9f802cf729393a2d6313e7e9
Andres Pastrana <rapastranac@gmail.com>
2026-04-19 17:00:05 -0600

    #30 Added arguments to build script to choose debug/release

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 WORKSPACE=$(cd "$(dirname "$0")/.." && pwd)
+BUILD_TYPE="${1:-Debug}"
 mkdir -p "$WORKSPACE/.build"
-(cd "$WORKSPACE/.build" && cmake -G "MSYS Makefiles" "$WORKSPACE" && make -j 20)
+(cd "$WORKSPACE/.build" && cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$WORKSPACE" && make -j 20)
