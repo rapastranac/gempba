4d7b5eb22840a809ffac8fa3c8f2fbe8da392c57
Andres Pastrana <rapastranac@gmail.com>
2026-04-19 17:00:05 -0600

    #30 Added arguments to build script to choose debug/release

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 WORKSPACE=$(cd "$(dirname "$0")/.." && pwd)
+BUILD_TYPE="${1:-Debug}"
 mkdir -p "$WORKSPACE/.build"
-(cd "$WORKSPACE/.build" && cmake "$WORKSPACE" && make -j 20)
+(cd "$WORKSPACE/.build" && cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$WORKSPACE" && make -j 20)
